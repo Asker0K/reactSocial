3232932b4dc779dc065607180dff453580fdddd5
Asker0K <askeee886@gmail.com>
2020-08-05 15:33:58 +0300

    v.1 RAW

---
+++ src/components/dialog/Dialog.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Dialog.module.css';
+
+const Dialogs = (props) => {
+    return (
+        <div className={classes.content}>Dialogs
+        </div>
+    );
+};
+
+export default Dialogs;
